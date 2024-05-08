df52b8a5463e1b64ee72d98a745c98472d21a3ba
Kepler <kepler3141@gmail.com>
2024-05-08 16:09:53 +0900

    REFACTOR: Structuralize model

--- src/main/api.py
+++ src/main/api.py
@@ -17,38 +17,38 @@ key: str = data["appKey"]
 app = FastAPI()
 
 
-@app.post("/path", response_model=model.Path)
-async def get_path(gpi: model.GetPathItem) -> JSONResponse:
+@app.post("/path", response_model=model.GetPath.response)
+async def get_path(p: model.GetPath.payload) -> JSONResponse:
     map_calc = map.MapCalculator(key)
 
-    start_coord = (gpi.startX, gpi.startY)
-    end_coord = (gpi.endX, gpi.endY)
+    start_coord = (p.startX, p.startY)
+    end_coord = (p.endX, p.endY)
 
     start_name = "starrt"
     end_name = "ennd"
 
-    path, time = map_calc.get_path(start_coord, end_coord, start_name, end_name, method=map.MovingMethods(gpi.method))
+    path, time = map_calc.get_path(start_coord, end_coord, start_name, end_name, method=map.MovingMethods(p.method))
 
-    path_time = model.Path(path=path, min_time=time)
+    path_time = model.GetPath.response(path=path, min_time=time)
 
     return JSONResponse(content=jsonable_encoder(path_time))
 
 
-@app.post("/min_time", response_model=model.Time)
-async def get_minimum_time(gmti: model.GetMinTimeItem) -> JSONResponse:
+@app.post("/min_time", response_model=model.GetMinimumTime.response)
+async def get_minimum_time(p: model.GetMinimumTime.payload) -> JSONResponse:
     """User Location + Server Location(도착 위치) -> min time"""
-    time = model.Time(time=10)
+    time = model.GetMinimumTime.response(time=10)
     return JSONResponse(content=jsonable_encoder(time))
 
 
-# @app.post("/sign_up", response_model=model.)
-# async def sign_up() -> JSONResponse:
+@app.post("/sign_up", response_model=model.SignUp.response)
+async def sign_up(user: model.SignUp.payload) -> JSONResponse:
     # db에 유저 정보 추가
-    # pass
+    pass
 
 
-# @app.post("/sign_in", response_model=model.)
-# async def sign_in() -> JSONResponse:
+@app.post("/sign_in", response_model=model.SignIn.response)
+async def sign_in(p: model.SignIn.payload) -> JSONResponse:
     # 만약 db에 유저가 있다면
     # jwt 토큰 발급
 
@@ -59,13 +59,13 @@ async def get_minimum_time(gmti: model.GetMinTimeItem) -> JSONResponse:
 
     # 앱에서는 jwt토큰이 있는 경우 자동으로 로그인을 시도한다
     # 실패하면 로그인 화면으로 이동
-    # pass
+    pass
 
 
-# @app.post("/sign_out", response_model=model.)
-# async def sign_out() -> JSONResponse:
+@app.post("/sign_out", response_model=model.SignOut.response)
+async def sign_out() -> JSONResponse:
     # jwt 토큰 폭파
-    # pass
+    pass
 
 
 if __name__ == "__main__":
--- src/main/model.py
+++ src/main/model.py
@@ -1,78 +1,91 @@
 from pydantic import BaseModel
 
 
-class GetPathItem(BaseModel):
-    startX: str
-    startY: str
-    endX: str
-    endY: str
-    method: str
+class GetPath:
+    class payload(BaseModel):
+        startX: str
+        startY: str
+        endX: str
+        endY: str
+        method: str
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "startX": "126.9246033",
-                    "startY": "33.45241976",
-                    "endX": "126.9041895",
-                    "endY": "33.4048969",
-                    "method": "walk"
+        model_config = {
+            "json_schema_extra": {
+                "examples": [
+                    {
+                        "startX": "126.9246033",
+                        "startY": "33.45241976",
+                        "endX": "126.9041895",
+                        "endY": "33.4048969",
+                        "method": "walk"
+                    }
+                ]
+            }
+        }
+
+    class response(BaseModel):
+        path: list
+        min_time: int
+
+        model_config = {
+            "json_schema_extra": {
+                "example": {
+                    "path": ["a", "b", "c"],
+                    "min_time": 10
                 }
-            ]
-        }
-    }
-
-
-class Path(BaseModel):
-    path: list
-    min_time: int
-
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                "path": ["a", "b", "c"],
-                "min_time": 10
             }
         }
-    }
 
 
-class GetMinTimeItem(BaseModel):
-    startX: str
-    startY: str
+class GetMinimumTime:
+    class payload(BaseModel):
+        startX: str
+        startY: str
 
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                "startX": "126.9246033",
-                "startY": "33.45241976"
+        model_config = {
+            "json_schema_extra": {
+                "example": {
+                    "startX": "126.9246033",
+                    "startY": "33.45241976"
+                }
             }
         }
-    }
 
+    class response(BaseModel):
+        time: int
 
-class Time(BaseModel):
-    time: int
-
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                "time": 10
+        model_config = {
+            "json_schema_extra": {
+                "example": {
+                    "time": 10
+                }
             }
         }
-    }
 
 
-class User(BaseModel):
-    id: str
-    username: str
+class SignUp:
+    class payload(BaseModel):
+        id: str
+        username: str
+        hashed_password: str
+
+    class response(BaseModel):
+        success: int
 
 
-class UserInDB(User):
-    hashed_password: str
+class SignIn:
+    class payload(BaseModel):
+        id: str
+        hashed_password: str
+
+    class response(BaseModel):
+        access_token: str
+        token_type: str
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+class SignOut:
+    class payload(BaseModel):
+        pass
 
+    class response(BaseModel):
+        pass
